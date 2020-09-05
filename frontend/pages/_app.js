47ed43a625df32301417314d89ae707f82b054cd
Woosik Koong <woosik.koong@gmail.com>
2020-09-05 18:03:39 +0900

    Add Custom App Component

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,19 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import Page from "../components/styles/Page";
+/**
+ * Persisting layout between page changes
+ * Keeping state when navigating pages
+ * Custom error handling using componentDidCatch
+ * Inject additional data into pages
+ * Add global CSS
+ *  
+ */
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <React.Fragment>
+      <Page />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
+};
 
 export default MyApp;
