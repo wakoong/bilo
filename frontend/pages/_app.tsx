57232d6b72e7b2c9122c101df783f68a7a7a1826
Woosik Koong <woosik.koong@gmail.com>
2020-09-08 22:36:45 -0700

    Add simple apollo client example

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,18 +1,24 @@
 import { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+
 import Page from "../components/Page";
+import client from "../lib/apolloClient";
+
 /**
  * Persisting layout between page changes
  * Keeping state when navigating pages
  * Custom error handling using componentDidCatch
  * Inject additional data into pages
  * Add global CSS
- *  
+ *
  */
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={client}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 };
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
+import { useQuery, gql } from "@apollo/client";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 
+const USERS = gql`
+  query {
+    users {
+      username
+    }
+  }
+`;
+
 const Home = () => {
+  const { loading, error, data } = useQuery(USERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log({ data });
   return (
     <React.Fragment>
       <Header />
